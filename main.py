c2efffcf734ccbae1c8d666a569c81ffb8932c32
Alan-Garci <de5troygamer21@gmail.com>
2025-05-20 17:52:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from exa_py import Exa
+
+exa = Exa('8883c011-9229-475c-9ac1-79adb7dd8331')
+
+query = input('Search here: ')
+
+response = exa.search(
+  query,
+  num_results=5,
+  type='keyword',
+  include_domains=['https://www.tiktok.com'],
+)
+
+for result in response.results:
+  print(f'Title: {result.title}')
+  print(f'URL: {result.url}')
+  print()
