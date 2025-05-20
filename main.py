4d4081b5e5d9f8facd4dcf3ead410d605b816280
Alan-Garci <de5troygamer21@gmail.com>
2025-05-20 18:01:35 -0400

    Secure API key by loading from .env

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from exa_py import Exa
+from dotenv import load_dotenv
+import os
 
-exa = Exa('8883c011-9229-475c-9ac1-79adb7dd8331')
+load_dotenv()
+exa = Exa(os.getenv('EXA_API_KEY'))
 
 query = input('Search here: ')
 
 response = exa.search(
-  query,
-  num_results=5,
-  type='keyword',
-  include_domains=['https://www.tiktok.com'],
+    query,
+    num_results=5,
+    type='keyword',
+    include_domains=['https://www.tiktok.com'],
 )
 
 for result in response.results:
-  print(f'Title: {result.title}')
-  print(f'URL: {result.url}')
-  print()
+    print(f'Title: {result.title}')
+    print(f'URL: {result.url}')
+    print()
